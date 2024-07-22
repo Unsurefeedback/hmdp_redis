48fc64d7aaed81c457dcea4da73f2fa668e71f7d
weihanqiang <2787883414@qq.com>
2024-07-22 11:05:17 +0800

    lua脚本，保证判断锁和释放锁操作打包在一起执行，保证原子性

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -39,14 +39,24 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unlock() {
-        //获取线程标示
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        //获取锁中的标示
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        //判断标示是否一致
-        if(threadId.equals(id)){
-            //释放锁
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+        // 调用lua脚本
+        // 传入的是集合，所以需要用Collections.singletonList
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
     }
+
+//    @Override
+//    public void unlock() {
+//        //获取线程标示
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        //获取锁中的标示
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        //判断标示是否一致
+//        if(threadId.equals(id)){
+//            //释放锁
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
+//    }
 }
